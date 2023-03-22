e5319e57280a95150c8562ccd87c416fdb6fb8d4
Oskár Adam Válent <xvalent@fi.muni.cz>
2023-03-22 16:23:17 +0100

    Repair broken project structure

--- youband-manager/src/main/java/cz/muni/fi/pa165/youbandmanager/YoubandManagerApplication.java
+++ youband-manager/src/main/java/cz/muni/fi/pa165/youbandmanager/YoubandManagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YoubandManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YoubandManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YoubandManagerApplication.class, args);
+    }
 
 }
