b643728a833d837b8335b546f82a53a85dc3c475
Patrik Čangel <xcangel@fi.muni.cz>
2023-04-16 00:55:02 +0200

    feat: adding test for rest controller

--- module-email/src/main/java/cz/muni/fi/pa165/moduleemail/rest/EmailRestController.java
+++ module-email/src/main/java/cz/muni/fi/pa165/moduleemail/rest/EmailRestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping(path = "api/email")
+@RequestMapping(path = "email")
 public class EmailRestController {
 
     private final EmailFacade emailFacade;
