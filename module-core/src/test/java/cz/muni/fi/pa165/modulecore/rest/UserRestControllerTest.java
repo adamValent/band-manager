9fa22f723bdf2b4e55014242dd5b572f8b5790f7
Oskár Adam Válent <xvalent@fi.muni.cz>
2023-04-16 13:52:18 +0200

    Adjust tests

--- module-core/src/test/java/cz/muni/fi/pa165/modulecore/rest/UserRestControllerTest.java
+++ module-core/src/test/java/cz/muni/fi/pa165/modulecore/rest/UserRestControllerTest.java
@@ -1,19 +1,26 @@
 package cz.muni.fi.pa165.modulecore.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.muni.fi.pa165.modulecore.api.SongDto;
 import cz.muni.fi.pa165.modulecore.api.UserDto;
 import cz.muni.fi.pa165.modulecore.data.enums.UserType;
 import cz.muni.fi.pa165.modulecore.data.model.User;
 import cz.muni.fi.pa165.modulecore.data.repository.UserRepository;
+import cz.muni.fi.pa165.modulecore.exception.ResourceNotFoundException;
 import cz.muni.fi.pa165.modulecore.mapper.UserMapper;
 import org.assertj.core.util.Lists;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -24,57 +31,61 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserRestControllerTest {
-    private final MockMvc mockMvc;
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
-    private final ObjectMapper objectMapper;
-
     @Autowired
-    public UserRestControllerTest(MockMvc mockMvc, UserRepository userRepository,
-                                  UserMapper userMapper, ObjectMapper objectMapper) {
-        this.mockMvc = mockMvc;
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
-        this.objectMapper = objectMapper;
-    }
+    private MockMvc mockMvc;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private UserRepository userRepository;
 
     @Test
     void findByIdOk() throws Exception {
-        UserDto expectedResponse = userMapper.mapToDto(userRepository.findById(10L).get());
-        String response = mockMvc.perform(get("/users/10"))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
+        User user = new User(1L, UserType.MANAGER, "name", "last", "mail", "pwd");
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        String response = mockMvc.perform(get("/users/1"))
+                                 .andExpect(status().isOk())
+                                 .andReturn()
+                                 .getResponse()
+                                 .getContentAsString();
         assertThat(objectMapper.readValue(response, UserDto.class),
-                is(equalTo(expectedResponse)));
+                   is(equalTo(userMapper.mapToDto(userRepository.findById(1L).get()))));
     }
 
     @Test
     void findByIdNotFound() throws Exception {
-        mockMvc.perform(get("/users/0"))
-                .andExpect(status().isNotFound());
+        Mockito.when(userRepository.findById(0L)).thenReturn(Optional.empty());
+        mockMvc.perform(get("/users/0")).andExpect(status().isNotFound());
     }
 
     @Test
     void getAll() throws Exception {
         String response = mockMvc.perform(get("/users"))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
+                                 .andExpect(status().isOk())
+                                 .andReturn()
+                                 .getResponse()
+                                 .getContentAsString();
         assertThat(objectMapper.readerForListOf(UserDto.class).readValue(response),
-                is(equalTo(userMapper.mapToList(Lists.newArrayList(userRepository.findAll())))));
+                   is(equalTo(userMapper.mapToList(Lists.newArrayList(userRepository.findAll())))));
     }
 
     @Test
     void createUserOk() throws Exception {
-        UserDto expectedResponse = userMapper.mapToDto(
-                new User(null, UserType.BAND_MEMBER, "John", "Person", "john@person.com",
-                        "password"));
-        String response = mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(expectedResponse)))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        assertThat(objectMapper.readValue(response, UserDto.class),
-                is(equalTo(expectedResponse)));
+        User user = new User(null, UserType.MANAGER, "name", "last", "mail", "pwd");
+        UserDto expectedResponse = userMapper.mapToDto(user);
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        String response = mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON)
+                                                        .content(objectMapper.writeValueAsString(
+                                                                expectedResponse)))
+                                 .andExpect(status().isOk())
+                                 .andReturn()
+                                 .getResponse()
+                                 .getContentAsString();
+
+        UserDto actual = objectMapper.readValue(response, UserDto.class);
+        expectedResponse.setId(actual.getId());
+        assertThat(actual, is(equalTo(expectedResponse)));
     }
 
     @Test
@@ -82,73 +93,78 @@ class UserRestControllerTest {
         JSONObject content = new JSONObject();
         content.put("test", "invalid");
         mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(content.toString()))
-                .andExpect(status().isBadRequest());
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content.toString()))
+               .andExpect(status().isBadRequest());
     }
 
     @Test
     void updateUserOk() throws Exception {
-        UserDto expectedResponse = userMapper.mapToDto(userRepository.findById(20L).get());
-        expectedResponse.setFirstName("John");
-        expectedResponse.setLastName("Person");
-        expectedResponse.setEmail("john@person.com");
-        String response = mockMvc.perform(put("/users/20")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(expectedResponse)))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        assertThat(objectMapper.readValue(response, UserDto.class),
-                is(equalTo(expectedResponse)));
+        User user = new User(1L, UserType.MANAGER, "name", "last", "mail", "pwd");
+        UserDto expectedResponse = userMapper.mapToDto(user);
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        Mockito.when(userRepository.existsById(user.getId())).thenReturn(true);
+        String response = mockMvc.perform(put("/users/1")
+                                                  .contentType(MediaType.APPLICATION_JSON)
+                                                  .content(objectMapper.writeValueAsString(expectedResponse)))
+                                 .andExpect(status().isOk())
+                                 .andReturn()
+                                 .getResponse()
+                                 .getContentAsString();
+        UserDto actual = objectMapper.readValue(response, UserDto.class);
+        assertThat(actual, is(equalTo(expectedResponse)));
     }
 
     @Test
     void updateUserBadRequest() throws Exception {
         JSONObject content = new JSONObject();
         content.put("test", "invalid");
-        mockMvc.perform(put("/users/20")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(content.toString()))
-                .andExpect(status().isBadRequest());
+        mockMvc.perform(put("/users/100")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content.toString()))
+               .andExpect(status().isBadRequest());
     }
 
     @Test
     void deleteUserOk() throws Exception {
-        mockMvc.perform(delete("/users/30"))
-                .andExpect(status().isOk());
+        Mockito.doNothing().when(userRepository).deleteById(1L);
+        mockMvc.perform(delete("/users/1")).andExpect(status().isOk());
     }
 
     @Test
     void deleteUserNotFound() throws Exception {
-        mockMvc.perform(delete("/users/0"))
-                .andExpect(status().isNotFound());
+        Mockito.doThrow(new ResourceNotFoundException()).when(userRepository).deleteById(0L);
+        mockMvc.perform(delete("/users/0")).andExpect(status().isNotFound());
     }
 
     @Test
     void registerUser() throws Exception {
-        UserDto user = userMapper.mapToDto(
-                new User(20L, UserType.BAND_MEMBER, "John", "Person", "john@person.com",
-                        "password"));
-        mockMvc.perform(post("/users/registration")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isOk());
+        UserDto user = userMapper.mapToDto(new User(20L,
+                                                    UserType.BAND_MEMBER,
+                                                    "John",
+                                                    "Person",
+                                                    "john@person.com",
+                                                    "password"));
+        mockMvc.perform(post("/users/registration").contentType(MediaType.APPLICATION_JSON)
+                                                   .content(objectMapper.writeValueAsString(user)))
+               .andExpect(status().isOk());
     }
 
     @Test
     void loginUser() throws Exception {
-        UserDto user = userMapper.mapToDto(
-                new User(20L, UserType.BAND_MEMBER, "John", "Person", "john@person.com",
-                        "password"));
-        mockMvc.perform(post("/users/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isOk());
+        UserDto user = userMapper.mapToDto(new User(20L,
+                                                    UserType.BAND_MEMBER,
+                                                    "John",
+                                                    "Person",
+                                                    "john@person.com",
+                                                    "password"));
+        mockMvc.perform(post("/users/login").contentType(MediaType.APPLICATION_JSON)
+                                            .content(objectMapper.writeValueAsString(user)))
+               .andExpect(status().isOk());
     }
 
     @Test
     void logoutUser() throws Exception {
-        mockMvc.perform(post("/users/logout"))
-                .andExpect(status().isOk());
+        mockMvc.perform(post("/users/logout")).andExpect(status().isOk());
     }
 }
