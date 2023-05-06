b1920c81c08698e1b89c043928dc57962261bf72
Oskár Adam Válent <xvalent@fi.muni.cz>
2023-05-06 20:00:37 +0200

    Add resource server

---
+++ resource-server/src/test/java/cz/muni/fi/pa165/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
