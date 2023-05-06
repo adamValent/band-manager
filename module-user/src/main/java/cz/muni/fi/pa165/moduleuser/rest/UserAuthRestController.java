d85397c5016f398b9cf197a045932805bfaeecb4
Oskár Adam Válent <xvalent@fi.muni.cz>
2023-05-06 20:37:24 +0200

    remove unused imports

--- module-user/src/main/java/cz/muni/fi/pa165/moduleuser/rest/UserAuthRestController.java
+++ module-user/src/main/java/cz/muni/fi/pa165/moduleuser/rest/UserAuthRestController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.server.resource.introspection.OAuth2IntrospectionAuthenticatedPrincipal;
 import org.springframework.web.bind.annotation.*;
 
